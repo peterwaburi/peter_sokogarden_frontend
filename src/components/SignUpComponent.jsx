ffa7c9e83ab367ab46498a45945f4c7469b26c08
peterwaburi <peterwaburi16511@gmail.com>
2026-03-12 14:03:06 +0300

    also signup

--- src/components/SignUpComponent.jsx
+++ src/components/SignUpComponent.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -7,12 +8,48 @@ const SignUpComponent = () => {
     let [email, updateEmail] = useState("")
     let [phone, updatePhone] = useState("")
     let [password, updatePassword] = useState("")
+
+    let [loading, setLoading] = useState("")
+    let [error, setError] = useState("")
+    let [success, setSuccess] = useState("")
+
+    const handleSubmit = (e) => {
+        // prevent default behaviour
+        e.preventDefault();
+
+        // notify user to wait
+        setLoading("Submitting Data! Please wait ....")
+
+        // confirm user input
+        console.log(username, email, phone, password);
+
+        // try send data to sever
+        try {
+            // create form data
+            const user_data = FormData();
+            user_data.append("username", username);
+            user_data.append("email", email);
+            user_data.append("phone", phone);
+            user_data.append("password", password);
+
+            // use axios tosend data to sever
+            const response = axios.post("https://kmuturi.alwaysdata.net/api/signup", user_data)
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+            setError(error.message);
+            setLoading("");
+        }
+    }
+
     return (
         <div className="row justify-content-center mt-4">
             <div className="col-md-6 card shadow p-4">
                 <h2>Sign Up</h2>
-                <p>current username : {username}</p>
-                <form >
+                <h5 className="text-warning">{loading}</h5>
+                <h5 className="text-danger">{error}</h5>
+
+                <form onSubmit={handleSubmit}>
 
                     <input
                         type="text"
@@ -20,28 +57,32 @@ const SignUpComponent = () => {
                         placeholder="enter username"
                         required
                         onChange={(e) => { updateUsername(e.target.value) }}
+                        value={username}
                     /><br />
 
                     <input
                         type="email"
                         className="form-control"
-                        placeholder="enter your email" 
+                        placeholder="enter your email"
                         required
-                        onChange={(e)=>{updateEmail(e.target.value)}}/><br />
+                        onChange={(e) => { updateEmail(e.target.value) }}
+                        value={email} /><br />
 
                     <input
                         type="tel"
                         className="form-control"
                         placeholder="enter phone number"
-                        required 
-                        onChange={(e)=>{updatePhone(e.target.value)}}/><br />
+                        required
+                        onChange={(e) => { updatePhone(e.target.value) }}
+                        value={phone} /><br />
 
                     <input
                         type="password"
                         className="form-control"
                         placeholder="enter your password"
                         required
-                        onChange={(e)=>{updatePassword(e.target.value)}} /><br />
+                        onChange={(e) => { updatePassword(e.target.value) }}
+                        value={password} /><br />
 
                     <button className="btn btn-dark">
                         Sign Up
